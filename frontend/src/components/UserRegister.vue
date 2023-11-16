261dc3ff0d9a054e860a922392edeb7c9236c0d3
Terer <terermike3@gmail.com>
2023-11-16 20:47:47 +0300

    Resized registration form and added Confirm password field for password validation

--- frontend/src/components/UserRegister.vue
+++ frontend/src/components/UserRegister.vue
@@ -1,16 +1,19 @@
 <template>
-  <el-form ref="form" :model="form" label-width="120px">
+  <el-form ref="form" :model="form" label-width="120px" size="small">
     <el-form-item label="Name" prop="name" :rules="nameRules">
-      <el-input v-model="form.name"></el-input>
+      <el-input v-model="form.name" class="input" size="small"></el-input>
     </el-form-item>
     <el-form-item label="Email" prop="email" :rules="emailRules">
-      <el-input v-model="form.email"></el-input>
+      <el-input v-model="form.email" class="input" size="small"></el-input>
     </el-form-item>
     <el-form-item label="Password" prop="password" :rules="passwordRules">
-      <el-input type="password" v-model="form.password"></el-input>
+      <el-input type="password" v-model="form.password" class="input" size="small"></el-input>
+    </el-form-item>
+    <el-form-item label="Confirm Password" prop="confirmPassword" :rules="confirmPasswordRules">
+      <el-input type="password" v-model="form.confirmPassword" class="input" size="small"></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="register">Register</el-button>
+      <el-button type="primary" @click="register" size="small">Register</el-button>
     </el-form-item>
   </el-form>
 </template>
@@ -31,29 +34,57 @@ export default {
       // Validation rules for each form field
       nameRules: [
         { required: true, message: 'Please enter your name', trigger: 'blur' },
-        // Add more rules as needed
       ],
       emailRules: [
         { required: true, message: 'Please enter your email', trigger: 'blur' },
         { type: 'email', message: 'Please enter a valid email address', trigger: ['blur', 'change'] },
-        // Add more rules as needed
       ],
       passwordRules: [
         { required: true, message: 'Please enter your password', trigger: 'blur' },
-        // Add more rules as needed
+      ],
+      confirmPasswordRules: [
+        { required: true, message: 'Please confirm your password', trigger: 'blur' },
+        { validator: this.validatePasswordMatch, trigger: 'blur' }
       ],
     };
   },
   methods: {
-    register() {
-      Csrf.getCookie().then(()=>{
-        User.register(this.form);
-      });
+  validatePasswordMatch(rule, value) {
+    return new Promise((resolve, reject) => {
+      if (value !== this.form.password) {
+        reject(new Error('Passwords do not match'));
+      } else {
+        resolve();
+      }
+    });
+  },
+  async register() {
+    try {
+      // Step 1: Get CSRF token
+      await Csrf.getCookie();
+      // Step 2: Validate the form
+      await this.$refs.form.validate();
+      // Step 3: If validation passes, call the backend API to register the user
+      const response = await User.register(this.form);
+      // Step 4: Handle the response from the backend
+      if (response.status === 200) {
+        // Registration successful, do something (e.g., redirect, show success message)
+        console.log('Registration successful');
+      } else {
+        // Registration failed, handle the error (e.g., show error message)
+        console.error('Registration failed', response.data);
+      }
+    } catch (error) {
+      // Handle any errors that occurred during the registration process
+      console.error('Error during registration', error);
     }
   }
+}
 };
 </script>
 
 <style scoped>
-/* Add scoped styles if needed */
+.input {
+  width: 50%;
+}
 </style>
